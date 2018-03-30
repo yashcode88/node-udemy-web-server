45d379f9d9f3e303b0a43aef00979ce6688d209f
yash <yash.avaghade@gmail.com>
2018-03-30 16:36:16 +0530

    exclude sitedata.json from code files

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 })
 
 app.get("/", (req, res) => {
-    var sdata1 = updateSiteData(sdata,req.url);
+    var sdata1 = updateSiteData(sdata, req.url);
     var data = { "name": "abc" };
     // var data = "hello."
     // res.send(data);
@@ -37,11 +37,11 @@ app.get("/", (req, res) => {
         pageTitle: "Home page",
         currentYear: new Date(),
         welcomeMessage: "Welcome my first node.js website.",
-        visitorCount:sdata1.count[req.url]
+        visitorCount: sdata1.count[req.url]
     });
 });
 app.get("/home", (req, res) => {
-    var sdata1 = updateSiteData(sdata,req.url);
+    var sdata1 = updateSiteData(sdata, req.url);
     var data = { "name": "abc" };
     // var data = "hello."
     // res.send(data);
@@ -49,41 +49,46 @@ app.get("/home", (req, res) => {
         pageTitle: "Home page",
         currentYear: new Date(),
         welcomeMessage: "Welcome my first node.js website.",
-        visitorCount:sdata1.count[req.url]
+        visitorCount: sdata1.count[req.url]
     });
 });
 
 app.get("/about", (req, res) => {
-    var sdata1 = updateSiteData(sdata,req.url);
+    var sdata1 = updateSiteData(sdata, req.url);
     // var data = {"name":"abc"};
     // va   r data = "hello."
     // res.send("About page.");
     res.render("about.hbs", {
         pageTitle: "About page",
         currentYear: new Date().toString(),
-        visitorCount:sdata1.count[req.url]
+        visitorCount: sdata1.count[req.url]
     });
 });
 
 app.get("/projects", (req, res) => {
-    var sdata1 = updateSiteData(sdata,req.url);
+    var sdata1 = updateSiteData(sdata, req.url);
     // var data = {"name":"abc"};
     // va   r data = "hello."
     // res.send("About page.");
     res.render("projects.hbs", {
         pageTitle: "Projects page",
         currentYear: new Date().toString(),
-        visitorCount:sdata1.count[req.url]
+        visitorCount: sdata1.count[req.url]
     });
 });
 
 var getSiteData = function () {
-    var data = JSON.parse( fs.readFileSync("sitedata.json","utf8") || '{"count":{}}');
+    var data = {"count":{}};
+    try{
+        data = JSON.parse(fs.readFileSync("sitedata.json", "utf8") || '{"count":{}}');
+    }catch(e){
+        // data = "";
+    }
     // console.log(JSON.stringify(data))
     return data
 }
 
-var updateSiteData = function (obj,url) {
+var updateSiteData = function (obj, url) {
     if (!obj.count[url]) obj.count[url] = 0;
     obj.count[url]++;
     fs.writeFileSync("sitedata.json", JSON.stringify(obj), (err) => {
